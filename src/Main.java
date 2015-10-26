32bd17122cff8ca1d849d297839a53b58fcba39e
nystromb <bnystrom@siu.edu>
2015-10-26 18:50:35 -0500

    anonymously creates a RequestHandler

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,16 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 
-
-public class Main {
-	public static final String CRLF = "\r\n\r\n";
-	
+public class Main {	
 	 public static void main(String[] args) {
 		ServerSettings.parse(args);
 		
 		try (ServerSocket server = new ServerSocket(ServerSettings.getPort())) {
 			while(true){
-				HttpServer runner = new HttpServer(server.accept());
-				runner.run();
+				new RequestHandler(server.accept()).start();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
-
 }
