58f2729683782400fa98a4440ca405b34425104d
nystromb <bnystrom@siu.edu>
2015-10-28 16:38:00 -0500

    Tests if headers are added to the request object

--- src/RequestFactoryTest.java
+++ src/RequestFactoryTest.java
@@ -18,7 +18,7 @@ public class RequestFactoryTest {
 		
 		Request req = RequestFactory.build(request);
 		
-		assertEquals("GET", req.getRequestMethod());
+		assertEquals("GET", req.getMethod());
 		assertEquals("/", req.getPath());
 		assertEquals("HTTP/1.1", req.getProtocolVersion());
 	}
@@ -29,7 +29,7 @@ public class RequestFactoryTest {
 		
 		Request req = RequestFactory.build(request);
 		
-		assertEquals("POST", req.getRequestMethod());
+		assertEquals("POST", req.getMethod());
 		assertEquals("/form", req.getPath());
 		assertEquals("HTTP/1.1", req.getProtocolVersion());
 	}
@@ -40,7 +40,7 @@ public class RequestFactoryTest {
 		
 		Request req = RequestFactory.build(request);
 		
-		assertEquals("PUT", req.getRequestMethod());
+		assertEquals("PUT", req.getMethod());
 		assertEquals("/form", req.getPath());
 		assertEquals("HTTP/1.1", req.getProtocolVersion());
 	}
@@ -51,8 +51,40 @@ public class RequestFactoryTest {
 		
 		Request req = RequestFactory.build(request);
 		
-		assertEquals("OPTIONS", req.getRequestMethod());
+		assertEquals("OPTIONS", req.getMethod());
 		assertEquals("/method_options", req.getPath());
 		assertEquals("HTTP/1.1", req.getProtocolVersion());
+	}
+	
+	@Test
+	public void testIfAddsOneHeader(){
+		String request = "OPTIONS /path HTTP/1.1\r\nUser-Agent: HTTPClient/1.1\r\n\r\n";
+		
+		Request req = RequestFactory.build(request);
+		
+		assertEquals("HTTPClient/1.1", req.headers.get("User-Agent"));
+		assertTrue(req.headers.containsKey("User-Agent"));
+	}
+	
+	@Test
+	public void testIfAddsMultipleHeaders(){
+		String request = "GET /path HTTP/1.1\r\nUser-Agent: HTTPClient/1.1\r\nContent-Length: 234\r\n\r\n";
+		
+		Request req = RequestFactory.build(request);
+		
+		assertEquals("HTTPClient/1.1", req.headers.get("User-Agent"));
+		assertTrue(req.headers.containsKey("User-Agent"));
+		
+		assertEquals("234", req.headers.get("Content-Length"));
+		assertTrue(req.headers.containsKey("Content-Length"));
+	}
+	
+	@Test
+	public void testIfAddsBodyToRequest(){
+		String request = "GET /path HTTP/1.1\r\nUser-Agent: HTTPClient/1.1\r\nContent-Length: 234\r\n\r\nSomebody=Data";
+		
+		Request req = RequestFactory.build(request);
+		
+		assertEquals("Somebody=Data", req.body);
 	}
 }
