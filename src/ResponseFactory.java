61eea9bee7db74ceeb8ab042f0f877cce03fa27c
nystromb <bnystrom@siu.edu>
2015-10-30 16:51:11 -0500

    added ResponseFactory

---
+++ src/ResponseFactory.java
@@ -0,0 +1,18 @@
+
+public class ResponseFactory {
+
+	public static String getResponse(Request request) {
+		String response = "";
+
+		response += request.getProtocolVersion() + " 200 OK\r\n";
+
+		if(request.getMethod().equals("OPTIONS"))
+			response += "Allow: GET,HEAD,POST,OPTIONS,PUT\r\n";
+
+		if(request.getBody() != null)
+			response += request.getBody();
+
+
+		return response + "\r\n";
+	}
+}
---
+++ src/ResponseFactoryTest.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ResponseFactoryTest {
+	Request request;
+
+	@Before
+	public void setUp() throws Exception {
+		request = new Request();
+	}
+
+	@Test
+	public void testGetResponse() {
+		request.setMethod("GET");
+		request.setPath("/");
+		request.setProtocolVersion("HTTP/1.1");
+
+		assertEquals("HTTP/1.1 200 OK\r\n\r\n", ResponseFactory.getResponse(request));
+	}
+
+	@Test
+	public void testIncludesAllowHeaderOnOptionsRequest() {
+		request.setMethod("OPTIONS");
+		request.setPath("/");
+		request.setProtocolVersion("HTTP/1.1");
+		String expectedResponse = "HTTP/1.1 200 OK\r\nAllow: GET,HEAD,POST,OPTIONS,PUT\r\n\r\n";
+
+		assertEquals(expectedResponse, ResponseFactory.getResponse(request));
+	}
+
+	@Test
+	public void testPutsDataInBody(){
+		request.setMethod("POST");
+		request.setPath("/form");
+		request.setProtocolVersion("HTTP/1.1");
+
+		request.setBody("some=data");
+
+		assertTrue(ResponseFactory.getResponse(request).contains("some=data"));
+	}
+}
