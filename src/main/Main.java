75fe3282019d27f0a91fb1facefbb1f125184024
nystromb <bnystrom@siu.edu>
2015-11-03 00:37:01 -0600

    Added brackets to while() loop

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,8 +7,9 @@ public class Main {
 	 public static void main(String[] args) {
 		ServerSettings.parse(args);
          try (ServerSocket serverSocket = new ServerSocket(ServerSettings.getPort())) {
-             while(true)
+             while(true) {
                  new HttpServer(serverSocket.accept()).run();
+             }
          } catch (IOException e) {
              System.out.println("Exception caught when trying to listen on port " + ServerSettings.getPort() + " or listening for a connection");
              System.out.println(e.getMessage());
