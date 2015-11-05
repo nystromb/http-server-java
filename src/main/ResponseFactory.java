067db0d7c5a9188686927d3b2ead44c0fd6c6278
nystromb <bnystrom@siu.edu>
2015-11-04 19:51:40 -0600

    Handles Posts, Gets, Puts, Deletes, and Option

--- src/main/ResponseFactory.java
+++ src/main/ResponseFactory.java
@@ -1,21 +1,43 @@
 package main;
 
+import java.util.HashMap;
+
 public class ResponseFactory {
+    private static HashMap<String, String> pathContent = new HashMap<>();
 
 	public static String getResponse(Request request) {
-		String response = "";
+		String response = request.getProtocolVersion();
 
-		response += request.getProtocolVersion() + " 200 OK\r\n";
+		response += " 200 OK\r\n";
 
-		if(request.getMethod().equals("OPTIONS"))
-			response += "Allow: GET,HEAD,POST,OPTIONS,PUT\r\n";
-
-        response += "\r\n";
-
-		if(request.getBody() != null)
-			response += request.getBody();
-
+        switch(request.getMethod()){
+            case "GET":
+                response += "Content-Length: " + getContent(request.getPath()).getBytes().length + "\r\n\r\n";
+                response += getContent(request.getPath());
+                break;
+            case "PUT":
+                pathContent.put(request.getPath(), request.getBody());
+                break;
+            case "POST":
+                pathContent.put(request.getPath(), request.getBody());
+                break;
+            case "DELETE":
+                pathContent.put(request.getPath(), null);
+                break;
+            case "OPTIONS":
+                response += "Allow: GET,HEAD,POST,OPTIONS,PUT\r\n\r\n";
+                break;
+        }
 
 		return response;
 	}
+
+    public static String getContent(String path){
+        String value = pathContent.get(path);
+        if(value == null){
+            return "";
+        }else {
+            return value;
+        }
+    }
 }
--- src/test/ResponseFactoryTest.java
+++ src/test/ResponseFactoryTest.java
@@ -8,6 +8,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.xml.ws.Response;
+
 public class ResponseFactoryTest {
 	Request request;
 
@@ -22,26 +24,85 @@ public class ResponseFactoryTest {
 		request.setPath("/");
 		request.setProtocolVersion("HTTP/1.1");
 
-		assertEquals("HTTP/1.1 200 OK\r\n\r\n", ResponseFactory.getResponse(request));
+		assertEquals("HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n", ResponseFactory.getResponse(request));
 	}
 
+    @Test
+     public void testPostDataThenGetData(){
+        request.setMethod("POST");
+        request.setPath("/form");
+        request.setProtocolVersion("HTTP/1.1");
+        request.setBody("some=data");
+
+        String res = ResponseFactory.getResponse(request);
+
+        assertTrue(res.contains("HTTP/1.1 200 OK\r\n"));
+
+        request.setMethod("GET");
+        request.setBody("");
+
+        res = ResponseFactory.getResponse(request);
+
+        assertEquals("HTTP/1.1 200 OK\r\nContent-Length: 9\r\n\r\nsome=data", res);
+    }
+
+    @Test
+    public void testPutDataThenGetData(){
+        request.setMethod("PUT");
+        request.setPath("/form");
+        request.setProtocolVersion("HTTP/1.1");
+        request.setBody("different=data");
+
+        String res = ResponseFactory.getResponse(request);
+
+        assertTrue(res.contains("HTTP/1.1 200 OK\r\n"));
+
+        request.setMethod("GET");
+        request.setBody("");
+
+        res = ResponseFactory.getResponse(request);
+
+        assertEquals("HTTP/1.1 200 OK\r\nContent-Length: 14\r\n\r\ndifferent=data", res);
+    }
+
+    @Test
+    public void testDeleteDataThenGetData(){
+        request.setMethod("PUT");
+        request.setPath("/form");
+        request.setProtocolVersion("HTTP/1.1");
+        request.setBody("different=data");
+
+        String res = ResponseFactory.getResponse(request);
+
+        assertTrue(res.contains("HTTP/1.1 200 OK\r\n"));
+
+        request.setMethod("GET");
+        request.setBody("");
+
+        res = ResponseFactory.getResponse(request);
+
+        assertEquals("HTTP/1.1 200 OK\r\nContent-Length: 14\r\n\r\ndifferent=data", res);
+
+        request.setMethod("DELETE");
+        request.setProtocolVersion("HTTP/1.1");
+
+        ResponseFactory.getResponse(request);
+
+        request.setMethod("GET");
+
+        res = ResponseFactory.getResponse(request);
+
+        assertEquals("HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n", res);
+
+    }
+
 	@Test
-	public void testIncludesAllowHeaderOnOptionsRequest() {
+	public void testOptionsRequestIncludesAllowHeaders() {
 		request.setMethod("OPTIONS");
 		request.setPath("/");
 		request.setProtocolVersion("HTTP/1.1");
 
 		String expectedResponse = "HTTP/1.1 200 OK\r\nAllow: GET,HEAD,POST,OPTIONS,PUT\r\n\r\n";
 		assertEquals(expectedResponse, ResponseFactory.getResponse(request));
-	}
-
-	@Test
-	public void testPutsDataInBody(){
-		request.setMethod("POST");
-		request.setPath("/form");
-		request.setProtocolVersion("HTTP/1.1");
-		request.setBody("some=data");
-
-		assertEquals("HTTP/1.1 200 OK\r\n\r\nsome=data", ResponseFactory.getResponse(request));
 	}
 }
