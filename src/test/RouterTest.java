925a628015e6a05fad21e4f834f9f79bf155455e
nystromb <bnystrom@siu.edu>
2015-11-06 14:05:05 -0600

    Router object keeps a list of routes

--- src/test/RouterTest.java
+++ src/test/RouterTest.java
@@ -1,7 +1,23 @@
 package test;
 
+import main.Router;
+import main.ServerSettings;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by nystrom on 11/5/15.
  */
 public class RouterTest {
+
+    public void testAddRoutes(){
+        Router.addResource("/");
+
+        assertTrue(Router.hasPath("/"));
+    }
+
+    public void testPathIsNotValid(){
+        assertFalse(Router.hasPath("/foobar"));
+    }
 }
