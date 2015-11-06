3959a8eabb2366824f7c93e697c8255b02ad7851
nystromb <bnystrom@siu.edu>
2015-11-06 14:06:01 -0600

    added Router

--- src/main/Router.java
+++ src/main/Router.java
@@ -1,7 +1,19 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by nystrom on 11/5/15.
  */
 public class Router {
+    static List<String> resources = new ArrayList();
+
+    public static void addResource(String resource){
+        resources.add(resource);
+    }
+
+    public static boolean hasPath(String resource){
+        return resources.contains(resource);
+    }
 }
