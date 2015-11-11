07bfdab2b50b0fa98dad26dce53d49ac96d9dea9
nystromb <bnystrom@siu.edu>
2015-11-11 14:30:00 -0600

    added @Test annotation

--- src/test/RouterTest.java
+++ src/test/RouterTest.java
@@ -10,13 +10,14 @@ import static org.junit.Assert.*;
  * Created by nystrom on 11/5/15.
  */
 public class RouterTest {
-
+    @Test
     public void testAddRoutes(){
         Router.addResource("/");
 
         assertTrue(Router.hasPath("/"));
     }
 
+    @Test
     public void testPathIsNotValid(){
         assertFalse(Router.hasPath("/foobar"));
     }
