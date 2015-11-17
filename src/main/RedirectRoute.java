bbb6278a0411f0b258b322fb2f6a559990fc90e1
nystromb <bnystrom@siu.edu>
2015-11-17 12:54:03 -0600

    Redirect handler

--- src/main/RedirectRoute.java
+++ src/main/RedirectRoute.java
@@ -1,7 +1,14 @@
 package main;
 
+import java.io.IOException;
+
 /**
  * Created by nystrom on 11/17/15.
  */
-public class RedirectRoute {
+public class RedirectRoute implements RequestHandler {
+
+    @Override
+    public String handle(Request request) throws IOException {
+        return "HTTP/1.1 302 Found\r\nLocation: http://localhost:5000/\r\n\r\n";
+    }
 }
