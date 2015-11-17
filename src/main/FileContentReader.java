a9c41b09a2973c430a1e7c060fdf65089fcd0fd3
nystromb <bnystrom@siu.edu>
2015-11-17 11:29:46 -0600

    FileContentReader handler

---
+++ src/main/FileContentReader.java
@@ -0,0 +1,26 @@
+package main;
+
+import com.sun.corba.se.spi.activation.Server;
+
+import java.io.IOException;
+import java.io.File;
+
+/**
+ * Created by nystrom on 11/16/15.
+ */
+public class FileContentReader implements RequestHandler {
+
+    @Override
+    public String handle(Request request) throws IOException {
+        StringBuffer response = new StringBuffer();
+
+        response.append("HTTP/1.1");
+        response.append(" ");
+        response.append("200 OK\r\n");
+        response.append("\r\n");
+
+        response.append(ServerReader.readFileContents(new File(ServerSettings.getDirectory(), request.getPath())));
+
+        return response.toString();
+    }
+}
