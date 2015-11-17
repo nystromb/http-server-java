95dc46ba2297184615984c14abbf3c9efc9d50ed
nystromb <bnystrom@siu.edu>
2015-11-17 11:51:28 -0600

    Creates links for each file

--- src/main/DirectoryReader.java
+++ src/main/DirectoryReader.java
@@ -12,7 +12,18 @@ public class DirectoryReader implements RequestHandler {
         StringBuffer response = new StringBuffer();
         response.append("HTTP/1.1 200 OK\r\n");
         response.append("\r\n");
-        response.append(ServerReader.readDirectoryContents(new File(ServerSettings.getDirectory(), request.getPath())));
+
+        response.append("<!DOCTYPE html>");
+        response.append("<html>");
+        response.append("<head>");
+        response.append("</head>");
+        response.append("<body>");
+        String [] listOfFiles = ServerReader.readDirectoryContents(new File(ServerSettings.getDirectory(), request.getPath())).split(" ");
+        for(String file : listOfFiles){
+            response.append("<li><a href=\"/" + file + "\">" + file + "</a></li>");
+        }
+        response.append("</body>");
+        response.append("</html>");
         return response.toString();
     }
 }
