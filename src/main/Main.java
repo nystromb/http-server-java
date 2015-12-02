2778c4a1e58f8ddd7ccee88caa088c3ed9197656
nystromb <bnystrom@siu.edu>
2015-12-02 14:27:23 -0600

    refactor Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,6 @@
 package main;
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.URISyntaxException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,21 +8,17 @@ import java.util.logging.Logger;
 public class Main {
     public static Logger logger = Logger.getLogger("ServerLogger");
 
-	 public static void main(String[] args) throws URISyntaxException, IOException {
-         ServerSettings.parse(args);
-         ServerSettings.setUpLogger();
-         ServerSettings.buildRoutes();
+    public static void main(String[] args) {
+        ServerSettings.parse(args);
+        ServerSettings.setUpLogger();
+        ServerSettings.buildRoutes();
 
-         try (ServerSocket serverSocket = new ServerSocket(ServerSettings.getPort())) {
-             logger.log(Level.INFO, "Server starting on port " + ServerSettings.getPort());
-
-             ExecutorService pool = Executors.newFixedThreadPool(15);
-             while(true) {
-                 Socket client = serverSocket.accept();
-                 pool.execute(new HttpServer(client));
-             }
-         } catch (IOException e) {
-             logger.log(Level.SEVERE, "Could not start server on port " + ServerSettings.getPort());
-         }
-     }
+        try {
+            HttpServer http = new HttpServer(ServerSettings.getPort());
+            logger.log(Level.INFO, "Server starting on port " + ServerSettings.getPort());
+            http.start();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "ERROR: Could not start server on port " + ServerSettings.getPort());
+        }
+    }
 }
