1f4df3a31739c4b21ed9733c0ba0bb7320e9a046
nystromb <bnystrom@siu.edu>
2015-12-04 11:24:45 -0600

    404 Not Found

--- src/main/Handlers/NotFoundHandler.java
+++ src/main/Handlers/NotFoundHandler.java
@@ -1,7 +1,20 @@
 package main.Handlers;
 
+import main.Request;
+import main.Response;
+
+import java.io.IOException;
+
 /**
  * Created by nystrom on 12/4/15.
  */
-public class NotFoundHandler {
+public class NotFoundHandler implements Requestable {
+    Response response = new Response();
+
+    @Override
+    public Response getResponse(Request request) throws IOException {
+        response.setStatus("404 Not Found");
+        response.setBody("Not Found".getBytes());
+        return response;
+    }
 }
--- src/test/Handlers/NotFoundHandlerTest.java
+++ src/test/Handlers/NotFoundHandlerTest.java
@@ -1,7 +1,34 @@
 package test.Handlers;
 
+import main.Handlers.NotFoundHandler;
+import main.Request;
+import main.Response;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by nystrom on 12/4/15.
  */
 public class NotFoundHandlerTest {
+    NotFoundHandler handler;
+
+    @Before
+    public void setUp(){
+        handler = new NotFoundHandler();
+    }
+
+    @Test
+    public void test() throws URISyntaxException, IOException {
+        Request request = new Request("GET", new URI("/foobar"), "HTTP/1.1");
+
+        Response response = handler.getResponse(request);
+
+        assertTrue(new String(response.toByteArray()).contains("404"));
+    }
 }
